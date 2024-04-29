bb1d11a8f0323f99dab612086f8edb72e76e8af6
Rohit <rohitmtak@gmail.com>
2024-04-29 09:20:31 +0530

    EDA and Feature Engineering

--- src/exception.py
+++ src/exception.py
@@ -1,13 +1,15 @@
 """
 Custom Exception Handling
+
 """
 
 import sys
+import logging
 from src.logger import logging
 
 # function to load the execution info and return the error message
 def error_message_detail(error, error_detail: sys):
-    #exc_info returns the info about error
+    # exc_info returns the info about error
     _,_,exc_tb = error_detail.exc_info()
     
     # get file name from the exc_info() function
